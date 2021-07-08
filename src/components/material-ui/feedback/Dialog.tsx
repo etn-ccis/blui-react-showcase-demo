6d1702ff4c77bfbe995932a5fcab5f28935d039d
daileytj <daileytj89@gmail.com>
2021-07-08 12:20:49 -0400

    Add material feedback component examples

--- src/components/material-ui/feedback/Dialog.tsx
+++ src/components/material-ui/feedback/Dialog.tsx
@@ -1,3 +1,138 @@
 import React from 'react';
+import {
+    useTheme,
+    Theme,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    AppBar,
+    Divider,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Toolbar,
+    Typography,
+    Slide,
+} from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/styles';
+import { TransitionProps } from '@material-ui/core/transitions/transition';
+import { Close } from '@material-ui/icons';
 
-export const DialogExample: React.FC = () => <div>Dialog Example Works!</div>;
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        button: {
+            marginBottom: theme.spacing(2),
+            width: 300,
+        },
+        appBar: {
+            position: 'relative',
+        },
+        title: {
+            marginLeft: theme.spacing(2.5),
+            flex: 1,
+        },
+    })
+);
+
+const Transition = React.forwardRef(
+    (props: TransitionProps & { children?: React.ReactElement }, ref: React.Ref<unknown>) => (
+        <Slide direction="up" ref={ref} {...props} />
+    )
+);
+
+export const DialogExample: React.FC = () => {
+    const theme = useTheme();
+    const classes = useStyles(theme);
+    const [open, setOpen] = React.useState(false);
+    const [fullDialogOpen, setFullDialogOpen] = React.useState(false);
+
+    const handleClickOpen = (): void => {
+        setOpen(true);
+    };
+
+    const handleClose = (): void => {
+        setOpen(false);
+    };
+
+    const handleFullDialogClickOpen = (): void => {
+        setFullDialogOpen(true);
+    };
+
+    const handleFullDialogClose = (): void => {
+        setFullDialogOpen(false);
+    };
+
+    return (
+        <div className={classes.container}>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen} className={classes.button}>
+                Open Dialog
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>{'Is PX Blue Your Favorite Design System?'}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        We assume this is rhetorical, but we wanted to ask anyway. The disagree button is disabled as a
+                        feature, not a bug. 😉
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary" disabled>
+                        Disagree
+                    </Button>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Agree
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Button variant="outlined" color="primary" onClick={handleFullDialogClickOpen} className={classes.button}>
+                Open Full Screen Dialog
+            </Button>
+            <Dialog fullScreen open={fullDialogOpen} onClose={handleFullDialogClose} TransitionComponent={Transition}>
+                <AppBar className={classes.appBar} color={'primary'}>
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" onClick={handleFullDialogClose} aria-label="close">
+                            <Close />
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            Select Your Favorite PX Blue Component
+                        </Typography>
+                        <Button autoFocus color="inherit" onClick={handleFullDialogClose}>
+                            Save
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+                <List>
+                    <ListItem button>
+                        <ListItemText
+                            primary="AppBar"
+                            secondary="An extension of the default AppBar from Material UI that can be resized / collapsed as the page is scrolled"
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <ListItemText
+                            primary="ChannelValue"
+                            secondary="A component used to display...a channel value (and units)"
+                        />
+                    </ListItem>
+                    <Divider />
+                    <ListItem button>
+                        <ListItemText
+                            primary="EmptyState"
+                            secondary="A component that can be used as a placeholder when no data is present"
+                        />
+                    </ListItem>
+                </List>
+            </Dialog>
+        </div>
+    );
+};
--- src/components/material-ui/feedback/Snackbar.tsx
+++ src/components/material-ui/feedback/Snackbar.tsx
@@ -1,3 +1,183 @@
 import React from 'react';
+import { useTheme, Theme, Button, IconButton, Snackbar } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/styles';
+import { Close } from '@material-ui/icons';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 
-export const SnackbarExample: React.FC = () => <div>Snackbar Example Works!</div>;
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        button: {
+            marginBottom: theme.spacing(2),
+            width: 300,
+        },
+    })
+);
+
+const Alert = (props: AlertProps): JSX.Element => <MuiAlert elevation={6} variant="filled" {...props} />;
+
+export const SnackbarExample: React.FC = () => {
+    const theme = useTheme();
+    const classes = useStyles(theme);
+    const [open, setOpen] = React.useState(false);
+    const [openError, setOpenError] = React.useState(false);
+    const [openWarning, setOpenWarning] = React.useState(false);
+    const [openInformation, setOpenInformation] = React.useState(false);
+    const [openSuccess, setOpenSuccess] = React.useState(false);
+
+    const handleClick = (): void => {
+        setOpen(true);
+    };
+
+    const handleClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+    const handleErrorClick = (): void => {
+        setOpenError(true);
+    };
+
+    const handleErrorClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenError(false);
+    };
+    const handleWarningClick = (): void => {
+        setOpenWarning(true);
+    };
+
+    const handleWarningClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenWarning(false);
+    };
+    const handleInformationClick = (): void => {
+        setOpenInformation(true);
+    };
+
+    const handleInformationClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenInformation(false);
+    };
+    const handleSuccessClick = (): void => {
+        setOpenSuccess(true);
+    };
+
+    const handleSuccessClose = (event: React.SyntheticEvent | React.MouseEvent, reason?: string): void => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenSuccess(false);
+    };
+
+    return (
+        <div className={classes.container}>
+            <Button variant="outlined" color="primary" onClick={handleClick} className={classes.button}>
+                Open Simple Snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={open}
+                autoHideDuration={5000}
+                onClose={handleClose}
+                message="Hey there! I'm a snackbar."
+                action={
+                    <React.Fragment>
+                        <Button color="secondary" size="small" onClick={handleClose}>
+                            UNDO
+                        </Button>
+                        <IconButton size="small" color="inherit" onClick={handleClose}>
+                            <Close fontSize="small" />
+                        </IconButton>
+                    </React.Fragment>
+                }
+            />
+
+            <Button variant="outlined" color="primary" onClick={handleErrorClick} className={classes.button}>
+                Open Error Snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={openError}
+                autoHideDuration={5000}
+                onClose={handleErrorClose}
+            >
+                <Alert onClose={handleErrorClose} severity="error">
+                    This is an error message!
+                </Alert>
+            </Snackbar>
+
+            <Button variant="outlined" color="primary" onClick={handleWarningClick} className={classes.button}>
+                Open Warning Snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={openWarning}
+                autoHideDuration={5000}
+                onClose={handleWarningClose}
+            >
+                <Alert onClose={handleWarningClose} severity="warning">
+                    This is a warning message!
+                </Alert>
+            </Snackbar>
+
+            <Button variant="outlined" color="primary" onClick={handleInformationClick} className={classes.button}>
+                Open Information Snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={openInformation}
+                autoHideDuration={5000}
+                onClose={handleInformationClose}
+            >
+                <Alert onClose={handleInformationClose} severity="info">
+                    This is an information message!
+                </Alert>
+            </Snackbar>
+
+            <Button variant="outlined" color="primary" onClick={handleSuccessClick} className={classes.button}>
+                Open Success Snackbar
+            </Button>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={openSuccess}
+                autoHideDuration={5000}
+                onClose={handleSuccessClose}
+            >
+                <Alert onClose={handleSuccessClose} severity="success">
+                    This is a success message!
+                </Alert>
+            </Snackbar>
+        </div>
+    );
+};
