8bf2aafad784d821e98e8e656380ccf5fcda5bdb
daileytj <daileytj89@gmail.com>
2021-07-12 13:04:11 -0400

    Add material-ui list example

--- src/components/material-ui/data-display/List.tsx
+++ src/components/material-ui/data-display/List.tsx
@@ -1,3 +1,69 @@
 import React from 'react';
+import { useTheme, Theme, Typography, Avatar, List, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/styles';
+import { Folder } from '@material-ui/icons';
 
-export const ListExample: React.FC = () => <div>List Example Works!</div>;
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            marginBottom: theme.spacing(4),
+        },
+        sectionTitle: {
+            marginBottom: theme.spacing(2),
+        },
+    })
+);
+
+const generate = (element: React.ReactElement): React.ReactElement[] =>
+    [0, 1, 2].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        })
+    );
+
+export const ListExample: React.FC = () => {
+    const theme = useTheme();
+    const classes = useStyles(theme);
+
+    return (
+        <>
+            <div className={classes.container}>
+                <Typography variant={'body1'} className={classes.sectionTitle}>
+                    Default
+                </Typography>
+
+                <List>
+                    {generate(
+                        <ListItem>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <Folder />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Single-line item" secondary={'Secondary text'} />
+                        </ListItem>
+                    )}
+                </List>
+            </div>
+
+            <div className={classes.container}>
+                <Typography variant={'body1'} className={classes.sectionTitle}>
+                    Dense
+                </Typography>
+
+                <List>
+                    {generate(
+                        <ListItem dense>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <Folder />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="Single-line item" secondary={'Secondary text'} />
+                        </ListItem>
+                    )}
+                </List>
+            </div>
+        </>
+    );
+};
