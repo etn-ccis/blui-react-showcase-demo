5af785937bf2f6bb416790ec992c9513b754e900
jeffvg <jeffvgreiner@eaton.com>
2021-09-22 10:09:39 -0400

    remove screen debug

--- src/__tests__/InfoListItemExample.test.tsx
+++ src/__tests__/InfoListItemExample.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import { CssBaseline } from '@material-ui/core';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
@@ -17,6 +17,5 @@ describe('Info list item example', () => {
                 </RTLThemeProvider>
             </Provider>
         );
-        // screen.debug();
     });
 });
