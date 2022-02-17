d7eaecd056a6acbd9d87da59a088c4369575bad7
Suraj Karambe <bkarambe@gmail.com>
2022-02-17 16:40:42 +0530

    Updated date pickers examples

--- src/components/material-ui/inputs/DateTime.tsx
+++ src/components/material-ui/inputs/DateTime.tsx
@@ -5,7 +5,6 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import MobileDatePicker from '@mui/lab/MobileDatePicker';
-import TimePicker from '@mui/lab/TimePicker';
 import MobileTimePicker from '@mui/lab/MobileTimePicker';
 import createStyles from '@mui/styles/createStyles';
 import makeStyles from '@mui/styles/makeStyles';
@@ -13,7 +12,7 @@ import makeStyles from '@mui/styles/makeStyles';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         dateTimeContainer: {
-            marginBottom: theme.spacing(1),
+            marginBottom: theme.spacing(2),
         },
     })
 );
@@ -31,7 +30,7 @@ export const DateTimeExample: React.FC = () => {
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <div className={classes.dateTimeContainer}>
                 <DatePicker
-                    label="Date Picker"
+                    label="Date picker inline"
                     value={selectedDate}
                     onChange={handleDateChange}
                     renderInput={(params): ReactElement => <TextField {...params} />}
@@ -39,15 +38,7 @@ export const DateTimeExample: React.FC = () => {
             </div>
             <div className={classes.dateTimeContainer}>
                 <MobileDatePicker
-                    label="Date Picker for mobile"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    renderInput={(params): ReactElement => <TextField {...params} />}
-                />
-            </div>
-            <div className={classes.dateTimeContainer}>
-                <TimePicker
-                    label="Time Picker"
+                    label="Date picker dialog"
                     value={selectedDate}
                     onChange={handleDateChange}
                     renderInput={(params): ReactElement => <TextField {...params} />}
@@ -55,7 +46,7 @@ export const DateTimeExample: React.FC = () => {
             </div>
             <div className={classes.dateTimeContainer}>
                 <MobileTimePicker
-                    label="Time Picker for mobile"
+                    label="Time picker"
                     value={selectedDate}
                     onChange={handleDateChange}
                     renderInput={(params): ReactElement => <TextField {...params} />}
