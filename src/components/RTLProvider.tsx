8af396ba8c3ca330287c997fdcd9ee3300b55237
Ekta Ghag <ektasawant99@gmail.com>
2022-03-15 17:36:09 +0530

    add cacheprovider for rtl

--- src/components/RTLProvider.tsx
+++ src/components/RTLProvider.tsx
@@ -1,38 +1,54 @@
 import React, { useEffect } from 'react';
-import { ThemeProvider, createTheme, StyledEngineProvider } from '@mui/material/styles';
-import { create } from 'jss';
-import rtl from 'jss-rtl';
-// import ThemeProvider from '@mui/styles/ThemeProvider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import jssPreset from '@mui/styles/jssPreset';
 import StylesProvider from '@mui/styles/StylesProvider';
 import * as BLUIThemes from '@brightlayer-ui/react-themes';
 import { useSelector } from 'react-redux';
 import { AppStore } from '../__types__';
+import rtl from 'jss-rtl';
+import { create } from 'jss';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
-// Configure JSS
-const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+document.body.setAttribute('dir', 'rtl');
+
+const jss = create({
+    plugins: [...jssPreset().plugins, rtl()],
+});
 
 export const RTLThemeProvider: React.FC = (props) => {
     const dir = useSelector((store: AppStore) => store.app.direction);
     const theme = useSelector((store: AppStore) => store.app.theme);
+
+    const cacheRtl = createCache({
+        key: dir === 'rtl' ? 'cssrtl' : 'cssltr',
+        prepend: true,
+        stylisPlugins: [rtlPlugin],
+    });
+
+    const cacheLtr = createCache({
+        key: dir === 'ltr' ? 'cssltr' : 'cssrtl',
+        prepend: true,
+        stylisPlugins: dir === 'ltr' ? undefined : [rtlPlugin],
+    });
+    
 
     useEffect(() => {
         document.body.dir = dir;
     }, [dir]);
 
     return (
-        <StylesProvider jss={jss}>
-            <StyledEngineProvider injectFirst>
-                <ThemeProvider
-                    theme={createTheme(
-                        Object.assign(theme === 'light' ? BLUIThemes.blue : BLUIThemes.blueDark, {
-                            direction: dir,
-                        })
-                    )}
-                >
-                    {props.children}
-                </ThemeProvider>
-            </StyledEngineProvider>
-        </StylesProvider>
+        <ThemeProvider
+            theme={createTheme(
+                Object.assign(theme === 'light' ? BLUIThemes.blue : BLUIThemes.blueDark, {
+                    direction: dir,
+                })
+            )}
+        >
+            <CacheProvider value={dir === 'ltr' ? cacheLtr : cacheRtl}>
+                <StylesProvider jss={jss}>{props.children}</StylesProvider>
+            </CacheProvider>
+        </ThemeProvider>
     );
 };
