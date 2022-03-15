f8df45d4bcc89c27941224b778509cbee45a2812
Ekta Ghag <ektasawant99@gmail.com>
2022-03-15 17:58:00 +0530

    fix prettier

--- src/components/RTLProvider.tsx
+++ src/components/RTLProvider.tsx
@@ -32,7 +32,6 @@ export const RTLThemeProvider: React.FC = (props) => {
         prepend: true,
         stylisPlugins: dir === 'ltr' ? undefined : [rtlPlugin],
     });
-    
 
     useEffect(() => {
         document.body.dir = dir;
