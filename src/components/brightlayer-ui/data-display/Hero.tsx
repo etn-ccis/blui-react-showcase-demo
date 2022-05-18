69b21b31cabe52c7f8c81585bb4b72c8f38961ee
Suraj Karambe <bkarambe@gmail.com>
2022-05-18 15:56:29 +0530

    Updated Hero example to use mui v5 style engine

--- src/components/brightlayer-ui/data-display/Hero.tsx
+++ src/components/brightlayer-ui/data-display/Hero.tsx
@@ -1,109 +1,91 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { useTheme, Theme } from '@mui/material/styles';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { ChannelValue, Hero, HeroBanner } from '@brightlayer-ui/react-components';
 import GradeA from '@brightlayer-ui/icons-mui/GradeA';
 import GradeB from '@brightlayer-ui/icons-mui/GradeB';
 import Temp from '@brightlayer-ui/icons-mui/Temp';
 import Schedule from '@mui/icons-material/Schedule';
 import * as colors from '@brightlayer-ui/colors';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        exampleRow: {
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            marginBottom: theme.spacing(2),
-        },
-        label: {
-            marginBottom: theme.spacing(1),
-        },
-        componentContainer: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            flex: 1,
-        },
-    })
+const ExampleRow = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    marginBottom: theme.spacing(2),
+}));
+
+const ComponentContainer = styled(Box)(() => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    flex: 1,
+}));
+
+const Label = styled(Typography)(({ theme }) => ({
+    marginBottom: theme.spacing(1),
+}));
+
+export const HeroExample: React.FC = () => (
+    <>
+        <ExampleRow>
+            <ComponentContainer>
+                <Label variant={'body2'}>Basic Usage</Label>
+                <Hero icon={<GradeA fontSize={'inherit'} />} label={'Efficiency'} sx={{flex: 1}} style={{ flex: 1 }} />
+            </ComponentContainer>
+            <ComponentContainer>
+                <Label variant={'body2'}>w/ Value and Units</Label>
+                <Hero
+                    icon={<GradeB fontSize={'inherit'} />}
+                    label={'Efficiency'}
+                    ChannelValueProps={{ value: 88, units: '%' }}
+                    style={{ flex: 1 }}
+                />
+            </ComponentContainer>
+        </ExampleRow>
+        <ExampleRow>
+            <ComponentContainer>
+                <Label variant={'body2'}>w/ Channel Value Children</Label>
+                <Hero icon={<Schedule fontSize={'inherit'} />} label={'Efficiency'} style={{ flex: 1 }}>
+                    <ChannelValue fontSize={'large'} value={1} units={'h'} />
+                    <ChannelValue fontSize={'large'} value={27} units={'m'} />
+                </Hero>
+            </ComponentContainer>
+            <ComponentContainer>
+                <Label variant={'body2'}>w/ Icon Colors</Label>
+                <Hero
+                    iconBackgroundColor={colors.red[500]}
+                    icon={<Temp fontSize={'inherit'} htmlColor={colors.white[50]} />}
+                    label={'Temperature'}
+                    ChannelValueProps={{ value: 38, units: '°C' }}
+                    style={{ flex: 1 }}
+                />
+            </ComponentContainer>
+        </ExampleRow>
+        <ComponentContainer>
+            <Label variant={'body2'}>Within a HeroBanner</Label>
+            <HeroBanner style={{ width: 376, border: `solid 1px ${colors.gray[50]}`, borderRadius: 4 }}>
+                <Hero
+                    icon={<GradeA fontSize={'inherit'} />}
+                    label={'Efficiency'}
+                    ChannelValueProps={{ value: '95', units: '%' }}
+                />
+                <Hero
+                    icon={<GradeB fontSize={'inherit'} />}
+                    label={'Efficiency'}
+                    ChannelValueProps={{ value: '88', units: '%' }}
+                />
+                <Hero icon={<Schedule fontSize={'inherit'} />} label={'Efficiency'}>
+                    <ChannelValue fontSize={'large'} value={1} units={'h'} />
+                    <ChannelValue fontSize={'large'} value={27} units={'m'} />
+                </Hero>
+                <Hero
+                    iconBackgroundColor={colors.red[500]}
+                    icon={<Temp fontSize={'inherit'} htmlColor={colors.white[50]} />}
+                    label={'Temperature'}
+                    ChannelValueProps={{ value: 38, units: '°C' }}
+                />
+            </HeroBanner>
+        </ComponentContainer>
+    </>
 );
-
-export const HeroExample: React.FC = () => {
-    const theme = useTheme();
-    const classes = useStyles(theme);
-
-    return (
-        <>
-            <div className={classes.exampleRow}>
-                <div className={classes.componentContainer}>
-                    <Typography variant={'body2'} className={classes.label}>
-                        Basic Usage
-                    </Typography>
-                    <Hero icon={<GradeA fontSize={'inherit'} />} label={'Efficiency'} style={{ flex: 1 }} />
-                </div>
-                <div className={classes.componentContainer}>
-                    <Typography variant={'body2'} className={classes.label}>
-                        w/ Value and Units
-                    </Typography>
-                    <Hero
-                        icon={<GradeB fontSize={'inherit'} />}
-                        label={'Efficiency'}
-                        ChannelValueProps={{ value: 88, units: '%' }}
-                        style={{ flex: 1 }}
-                    />
-                </div>
-            </div>
-            <div className={classes.exampleRow}>
-                <div className={classes.componentContainer}>
-                    <Typography variant={'body2'} className={classes.label}>
-                        w/ Channel Value Children
-                    </Typography>
-                    <Hero icon={<Schedule fontSize={'inherit'} />} label={'Efficiency'} style={{ flex: 1 }}>
-                        <ChannelValue fontSize={'large'} value={1} units={'h'} />
-                        <ChannelValue fontSize={'large'} value={27} units={'m'} />
-                    </Hero>
-                </div>
-                <div className={classes.componentContainer}>
-                    <Typography variant={'body2'} className={classes.label}>
-                        w/ Icon Colors
-                    </Typography>
-                    <Hero
-                        iconBackgroundColor={colors.red[500]}
-                        icon={<Temp fontSize={'inherit'} htmlColor={colors.white[50]} />}
-                        label={'Temperature'}
-                        ChannelValueProps={{ value: 38, units: '°C' }}
-                        style={{ flex: 1 }}
-                    />
-                </div>
-            </div>
-            <div className={classes.componentContainer}>
-                <Typography variant={'body2'} className={classes.label}>
-                    Within a HeroBanner
-                </Typography>
-                <HeroBanner style={{ width: 376, border: `solid 1px ${colors.gray[50]}`, borderRadius: 4 }}>
-                    <Hero
-                        icon={<GradeA fontSize={'inherit'} />}
-                        label={'Efficiency'}
-                        ChannelValueProps={{ value: '95', units: '%' }}
-                    />
-                    <Hero
-                        icon={<GradeB fontSize={'inherit'} />}
-                        label={'Efficiency'}
-                        ChannelValueProps={{ value: '88', units: '%' }}
-                    />
-                    <Hero icon={<Schedule fontSize={'inherit'} />} label={'Efficiency'}>
-                        <ChannelValue fontSize={'large'} value={1} units={'h'} />
-                        <ChannelValue fontSize={'large'} value={27} units={'m'} />
-                    </Hero>
-                    <Hero
-                        iconBackgroundColor={colors.red[500]}
-                        icon={<Temp fontSize={'inherit'} htmlColor={colors.white[50]} />}
-                        label={'Temperature'}
-                        ChannelValueProps={{ value: 38, units: '°C' }}
-                    />
-                </HeroBanner>
-            </div>
-        </>
-    );
-};
