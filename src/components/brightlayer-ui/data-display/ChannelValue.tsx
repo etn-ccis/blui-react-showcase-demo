bde8e099c2de20a4090ddc1161c9656b25dc3016
Suraj Karambe <bkarambe@gmail.com>
2022-05-18 14:33:27 +0530

    Updated channel value example to use mui v5 style engine

--- src/components/brightlayer-ui/data-display/ChannelValue.tsx
+++ src/components/brightlayer-ui/data-display/ChannelValue.tsx
@@ -1,73 +1,55 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { useTheme, Theme } from '@mui/material/styles';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
+import { styled } from '@mui/material/styles';
 import { ChannelValue } from '@brightlayer-ui/react-components';
 import TrendingUp from '@mui/icons-material/TrendingUp';
 import * as colors from '@brightlayer-ui/colors';
 import { useSelector } from 'react-redux';
 import { AppStore } from '../../../__types__';
-import clsx from 'clsx';
+import Box from '@mui/material/Box';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        sectionTitle: {
-            marginBottom: theme.spacing(2),
-        },
-        componentContainer: {
-            display: 'flex',
-            flexDirection: 'column',
-            flex: 1,
-            marginBottom: theme.spacing(4),
-        },
-        iconFlip: {
-            transform: 'scaleX(-1)',
-        },
-    })
-);
+const ComponentContainer = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    marginBottom: theme.spacing(4),
+}));
+
+const SectionTitle = styled(Typography)(({ theme }) => ({
+    marginBottom: theme.spacing(2),
+}));
 
 export const ChannelValueExample: React.FC = () => {
-    const theme = useTheme();
-    const classes = useStyles(theme);
     const direction = useSelector((store: AppStore) => store.app.direction);
     const rtl = direction === 'rtl';
 
     return (
         <>
-            <div className={classes.componentContainer}>
-                <Typography variant={'body1'} className={classes.sectionTitle}>
-                    Basic Usage
-                </Typography>
+            <ComponentContainer>
+                <SectionTitle variant={'body1'}>Basic Usage</SectionTitle>
                 <ChannelValue value={'123'} />
-            </div>
-            <div className={classes.componentContainer}>
-                <Typography variant={'body1'} className={classes.sectionTitle}>
-                    w/ Units
-                </Typography>
+            </ComponentContainer>
+            <ComponentContainer>
+                <SectionTitle variant={'body1'}>w/ Units</SectionTitle>
                 <ChannelValue value={'123'} units={'hz'} />
-            </div>
-            <div className={classes.componentContainer}>
-                <Typography variant={'body1'} className={classes.sectionTitle}>
-                    w/ Icon
-                </Typography>
+            </ComponentContainer>
+            <ComponentContainer>
+                <SectionTitle variant={'body1'}>w/ Icon</SectionTitle>
                 <ChannelValue
                     value={'123'}
                     units={'hz'}
-                    icon={<TrendingUp htmlColor={colors.red[500]} className={clsx({ [classes.iconFlip]: rtl })} />}
+                    icon={<TrendingUp htmlColor={colors.red[500]} sx={rtl ? { transform: 'scaleX(-1)' } : {}} />}
                 />
-            </div>
-            <div className={classes.componentContainer}>
-                <Typography variant={'body1'} className={classes.sectionTitle}>
-                    w/ Custom Font Size
-                </Typography>
+            </ComponentContainer>
+            <ComponentContainer>
+                <SectionTitle variant={'body1'}>w/ Custom Font Size</SectionTitle>
                 <ChannelValue
                     value={'123'}
                     units={'hz'}
                     fontSize={24}
-                    icon={<TrendingUp htmlColor={colors.red[500]} className={clsx({ [classes.iconFlip]: rtl })} />}
+                    icon={<TrendingUp htmlColor={colors.red[500]} sx={rtl ? { transform: 'scaleX(-1)' } : {}} />}
                 />
-            </div>
+            </ComponentContainer>
         </>
     );
 };
