8f6c2099e9c61826b81c3d6c69c2558aca276b50
daileytj <daileytj89@gmail.com>
2022-11-21 12:55:12 -0500

    Use icons and themes alpha packages

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@
  This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
  **/
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import * as serviceWorker from './serviceWorker';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Provider } from 'react-redux';
@@ -17,15 +17,19 @@ import { RTLThemeProvider } from './components/RTLProvider';
 import '@brightlayer-ui/react-themes/open-sans';
 
 // eslint-disable-arrow-body-style
+const container = document.getElementById('root');
 
-ReactDOM.render(
+if (!container) throw new Error('Root Element was not found in the DOM');
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
     <Provider store={store}>
         <RTLThemeProvider>
             <CssBaseline />
             <MainRouter />
         </RTLThemeProvider>
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
