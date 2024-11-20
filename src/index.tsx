3151819510ae6f350e856a9c692d03bbb9ba263a
jeffgreiner-eaton <jeffvgreiner@eaton.com>
2024-11-20 12:48:43 -0500

    eslint ignore

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@
 
  This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
  **/
+/* eslint-disable */
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 // import * as {serviceWorker} from './serviceWorker';
