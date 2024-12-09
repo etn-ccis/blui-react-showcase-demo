d529829ae2444d14a58e7874d27a097a16a8f218
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2024-12-09 14:26:52 +0530

    Fixed lint issue

--- src/components/material-ui/data-display/Table.tsx
+++ src/components/material-ui/data-display/Table.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTheme, lighten } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
